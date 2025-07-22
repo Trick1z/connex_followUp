76ee62cc459b025f8403b1ccb93e845689e75076
trick1z <tiker.zone.teen@gmail.com>
2025-07-22 16:14:00 +0700

    add login page

--- src/app/views/pages/login/login.component.ts
+++ src/app/views/pages/login/login.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
+  standalone: true,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  ngOnInit(): void {}
 
+  constructor() {}
+
+  // valuable
+
+  data: any = {};
+
+  test() {
+    console.log(this.data);
+  }
 }
