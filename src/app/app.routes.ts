0c6bebd784b35b4524ed4e57a1d3a28be8f798dd
trick1z <tiker.zone.teen@gmail.com>
2025-07-22 16:27:01 +0700

    add route path [login , game/score ]

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,43 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './views/pages/login/login.component';
+import { WordScoreComponent } from './views/games/word-score/word-score.component';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+
+
+  { path: 'login', component: LoginComponent },
+  // { path: 'register', component: LoginComponent },
+
+
+
+
+
+
+
+
+
+  { path: 'games/word', component: WordScoreComponent },
+
+
+
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' }
+
+
+  // {
+  //   path: 'games',
+  //   loadChildren: () =>
+  //     import('./games/games.module').then((m) => m.GamesModule)
+  // },
+  // {
+  //   path: 'views',
+  //   loadChildren: () =>
+  //     import('./views/views.module').then((m) => m.ViewsModule)
+  // },
+  // { path: '', redirectTo: 'views/login', pathMatch: 'full' },
+  // { path: '**', redirectTo: 'views/login' }
+
+
+
 
 ];
