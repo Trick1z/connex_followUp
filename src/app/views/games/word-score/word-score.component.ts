9365b60b8b4186c1eacc5261be1c7a20907a4886
trick1z <tiker.zone.teen@gmail.com>
2025-07-23 18:31:28 +0700

    add game/wordScoring page, mockup data

--- src/app/views/games/word-score/word-score.component.ts
+++ src/app/views/games/word-score/word-score.component.ts
@@ -1,11 +1,66 @@
-import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-word-score',
-  imports: [],
+  imports: [NgIf, NgFor, FormsModule],
+  standalone: true,
   templateUrl: './word-score.component.html',
-  styleUrl: './word-score.component.scss'
+  styleUrl: './word-score.component.scss',
 })
-export class WordScoreComponent {
+export class WordScoreComponent implements OnInit {
+  ngOnInit(): void {
+    this.total_score = this.sum_sc()
+  }
 
+  constructor() {}
+
+  GameStart: boolean = false;
+  IsVip: boolean = true
+
+  startAndStopGame() {
+    this.GameStart = !this.GameStart;
+  }
+
+  // valuables
+
+  words: WordScore[] = [
+    { word: 'left1', score: 21 },
+    { word: 'lefttt2', score: 22 },
+    { word: 'leftttt3', score: 23 },
+    { word: 'lefttttttt4', score: 24 },
+  ];
+
+  getWord: string | null = null;
+
+  // word : Array<string>  = []
+
+  total_score: number = 0;
+
+  // sum total score
+  sum_sc(index: number = this.words.length): number {
+    if (index == 0) {
+      return 0;
+    }
+
+    return this.words[index - 1].score + this.sum_sc(index - 1);
+  }
+
+  star(length : number){
+    return '*'.repeat(length)
+  }
+
+  //setvip
+  setVIP(){
+    this.IsVip = !this.IsVip
+
+  }
+
+
+}
+
+interface WordScore {
+  word: string;
+  score: number;
 }
