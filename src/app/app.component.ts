5402bfed85114af462ce3652de22ff2190143501
trick1z <tiker.zone.teen@gmail.com>
2025-07-23 19:16:56 +0700

    add navBar  to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Route, Router, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './views/navbar/navbar.component';
+import { NgIf } from '@angular/common';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  showNavbar: boolean = false;
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      // แสดง navbar เฉพาะหน้า /games
+      this.showNavbar = event.urlAfterRedirects !== '/login';
+    });
+  }
+
+  constructor(private router: Router) {}
+
   title = 'app';
+
 }
