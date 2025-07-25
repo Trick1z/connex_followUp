56274cdf3a5ea78888fddce2b9bebb97318aaaf9
trick1z <tiker.zone.teen@gmail.com>
2025-07-25 11:19:49 +0700

    Add route guard to check user authentication before navigation

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,26 +1,52 @@
-// import { CanActivateFn } from '@angular/router';
+// // import { CanActivateFn } from '@angular/router';
 
-// export const authGuard: CanActivateFn = (route, state) => {
-//   return true;
-// };
+// // export const authGuard: CanActivateFn = (route, state) => {
+// //   return true;
+// // };
+
+
+// import { Injectable } from '@angular/core';
+// import { CanActivate, Router } from '@angular/router';
+
+// @Injectable({ providedIn: 'root' })
+// export class AuthGuard implements CanActivate {
+
+//   constructor(private router: Router) {}
+
+//   canActivate(): boolean {
+//     const isLoggedIn = !!sessionStorage.getItem('token'); // หรือเช็คจาก authService
+
+//     if (!isLoggedIn) {
+//       this.router.navigate(['/login']); // ส่งไปหน้า login
+//       return false;
+//     }else{
+//       this.router.navigate(['/register']); // ส่งไปหน้า login
+//       return false;
+//     }
+
+//     return true;
+//   }
+// }
 
 
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root'
+})
 export class AuthGuard implements CanActivate {
 
   constructor(private router: Router) {}
 
   canActivate(): boolean {
-    const isLoggedIn = !!sessionStorage.getItem('token'); // หรือเช็คจาก authService
+    const token = sessionStorage.getItem('token');
 
-    if (!isLoggedIn) {
-      this.router.navigate(['/login']); // ส่งไปหน้า login
+    if (token) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
       return false;
     }
-
-    return true;
   }
 }
